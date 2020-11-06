8577907b6b554bbf82871648b09de76eab4351e9
Lucasoviedo <lucasoviedolucas@gmail.com>
2020-11-06 01:31:58 -0300

    funcionalidades

---
+++ turnos-medicos-svelte/src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Home from "./Home/Home.svelte";
+	import Client from "./Client/Client.svelte";
+
+	export let page;
+
+
+	function funcioncita() {
+		if (page == "Home") {
+			page = "Mis turnos";
+		}
+		else {
+			page = "Home";
+		}
+	}
+
+</script>
+
+
+
+
+
+<main>
+	<Client />
+
+	{#if page === "Home"}
+	<Home />
+	{:else if page === "Mis turnos"}
+	<Client />
+	​​
+	{/if}
+
+	<button type="button" on:click={funcioncita}>Change</button>
+
+</main>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		/*margin: 0 auto;*/
+		margin: 0 !important;
+		padding: 0 !important;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
